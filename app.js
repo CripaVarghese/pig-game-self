1dc29cc7b00b721051734e3087d9b64c9855374a
CripaVarghese <cripav96@gmail.com>
2021-08-29 23:32:28 +0530

    issue in currentScore0

--- app.js
+++ app.js
@@ -14,71 +14,116 @@ const dice1=document.getElementById("dice-1");
 // const score1=document.getElementById("score-1");
 
 //default
-var currentScore0=0;
-var currentScore1=0;
-var totalScore0=0;
-var totalScore1=0;
+let currentScore0=0;
+let currentScore1=0;
+let totalScore0=0;
+let totalScore1=0;
 dice1.style.display="none";
 current0.innerHTML=0;
 current1.innerHTML=0;
 score0.innerHTML=0;
 score1.innerHTML=0;      
 
-const roll= () => {
-    btnRoll.addEventListener("click", ()=>{
-            const diceNumber=Math.floor(Math.random()*6+1);
-            dice1.src=`dice-${diceNumber}.png`;
-            dice1.innerHTML=`dice-${diceNumber}.png`;        
-            dice1.style.display="block";    
-            console.log("hii");
-    });
-};
-roll();
-
-const firstPlayer = (dice) => {
-    
-
-    if (dice!=1) {
-        currentScore0=currentScore0+dice;
-        current0.innerHTML=currentScore0;
-        console.log(currentScore0);
-        
-        btnHold.addEventListener("click", ()=>{
-            console.log(currentScore0);        
-            score0.innerHTML=currentScore0;        
-            current0.innerHTML=0;
-            secondPlayer();
-        });
-    } else if(diceNumber==1) {
-        score0.innerHTML=0;        
-        current0.innerHTML=0;
-        secondPlayer();
-    }
+// const roll= () => {
    
+// };
+// roll(diceNumber);
+
+const firstPlayer = () => {
+    
+    btnRoll.addEventListener("click", ()=>{
+        const diceNumber=Math.floor(Math.random()*6+1);
+        dice1.src=`dice-${diceNumber}.png`;
+        dice1.innerHTML=`dice-${diceNumber}.png`;        
+        dice1.style.display="block";    
+
+        currentScore1=0;
+                current1.innerHTML=currentScore1;
+                console.log(totalScore1);
+        
+        if (diceNumber!=1) {
+            // console.log(currentScore0)
+            currentScore0=currentScore0+diceNumber;
+            // console.log(currentScore0)
+            current0.innerHTML=currentScore0;
+            // console.log(currentScore0)
+            
+            btnHold.addEventListener("click", ()=>{
+                totalScore0=totalScore0+currentScore0;
+                score0.innerHTML=totalScore0; 
+                console.log(totalScore0)       
+                currentScore0=0;
+                current0.innerHTML=currentScore0;
+
+                currentScore1=0;
+                current1.innerHTML=currentScore1;
+                console.log(currentScore1)
+                secondPlayer();
+                
+                
+            });
+        } 
+        // else if(diceNumber==1) {
+        //     currentScore0=0;
+        //     current0.innerHTML=currentScore0;
+        //     totalScore0=0
+        //     score0.innerHTML=totalScore0;        
+            
+        //     secondPlayer();
+        // }
+    }); 
 };
 
 firstPlayer();
  
+const secondPlayer = () => {
+    dice1.style.display="none";
 
-// const secondPlayer = (dice) => {
-//     if (dice!=1) {
-//         currentScore1=currentScore1+dice;
-//         current1.innerHTML=currentScore1;
-//         console.log(currentScore1);
+    currentScore1=0;
+                current1.innerHTML=currentScore1;
+    
+    btnRoll.addEventListener("click", ()=>{
+        const diceNumber=Math.floor(Math.random()*6+1);
+        dice1.src=`dice-${diceNumber}.png`;
+        dice1.innerHTML=`dice-${diceNumber}.png`;        
+        dice1.style.display="block";    
+
+        currentScore0=0;
+        current0.innerHTML=currentScore0;
+
+        console.log(currentScore1)
+
         
-//         btnHold.addEventListener("click", ()=>{
-//             console.log(currentScore1);        
-//             score1.innerHTML=currentScore1;        
-//             current1.innerHTML=0;
-//             secondPlayer();
-//         });
-//     } else if(dice==1) {
-//         score1.innerHTML=0;        
-//         current1.innerHTML=0;
-//         secondPlayer();
-//     };
+        if (diceNumber!=1) {
 
-//  };
+            
+            console.log(currentScore1)
+            console.log(diceNumber);
+            currentScore1=currentScore1+diceNumber;
+            console.log(diceNumber)
+            console.log(currentScore1)
+            current1.innerHTML=currentScore1;
+            
+            btnHold.addEventListener("click", ()=>{
+                totalScore1=totalScore1+currentScore1;
+                score1.innerHTML=totalScore1; 
+                console.log()       
+                currentScore1=0;
+                current1.innerHTML=currentScore1;
+
+                firstPlayer();
+            });
+        } 
+        // else if(diceNumber==1) {
+        //     currentScore1=0;
+        //     current1.innerHTML=currentScore1;
+        //     totalScore1=0
+        //     score1.innerHTML=totalScore1;        
+            
+        //     firstPlayer();
+        // }
+    }); 
+};
 
 
 
@@ -97,7 +142,3 @@ firstPlayer();
 
 
 
-
-//initial values
-
-// let name =[0,1];
